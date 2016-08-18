d3fbadddfdf3b324ed5d35d4684ba64237f97cce
Krell <KrellTheHuman@gmail.com>
2016-08-17 20:31:52 -0700

    update button text

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//
+
+// update output box
+var updateOutput = function () {
+    var cornerType = $("input:radio[name='corner-option']:checked").val();
+    var red = $("#red-amount").val();
+    var green = $("#green-amount").val();
+    var blue = $("#blue-amount").val();
+    var fontSize = $("#text-size").val();
+    // $("#code-to-copy").html("Corner Type: " + cornerType + "<br/>Color: ");
+    $("#fancy-button").css("background-color", "rgb(" + red + ", " + green + ", " + blue + ")");
+    $("#fancy-button").css("font-size", fontSize + "px");
+};
+
+// when user clicks anywhere in body, update output
+$("body").on("click", function () {
+    updateOutput();
+});
+
