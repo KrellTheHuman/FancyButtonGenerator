34308996d690da712aea2e01158ea06a829392db
jyxx22 <jyxx22@gmail.com>
2016-08-18 13:16:22 +0900

    button size update

--- script.js
+++ script.js
@@ -8,17 +8,27 @@ var updateOutput = function () {
     var fontSize = $("#text-size").val();
     var buttonText = $("#button-text").val();
     var fancyButton = $("#fancy-button");
+    var buttonSize = $("#button-size").val();
     fancyButton.css("background-color", "rgb(" + red + ", " + green + ", " + blue + ")");
     fancyButton.css("font-size", fontSize + "px");
     fancyButton.text(buttonText);
+    if (buttonSize === "button-small") {
+        fancyButton.css("padding", "5px");
+    } else if (buttonSize === "button-med") {
+        fancyButton.css("padding", "15px");
+    } else {
+        fancyButton.css("padding", "30px");
+    }
     if (cornerType === "square") {
         fancyButton.css("border-radius", "0");
+        fancyButton.css("height", "auto");
     } else if (cornerType === "rounded") {
         fancyButton.css("border-radius", "12px");
+        fancyButton.css("height", "auto");
     } else {
         fancyButton.css("border-radius", "100%");
+        fancyButton.css("height", fancyButton.css("width"));
     }
-    fancyButton.css("height", fancyButton.css("width"));
 };
 
 // when user clicks anywhere in body, update output
